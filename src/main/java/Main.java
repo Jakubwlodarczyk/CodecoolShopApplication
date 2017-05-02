633e9908e152a9c77597c1b570c7867f76e7d4dd
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-02 15:09:33 +0200

    Add try catch for creating table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,19 @@
 import com.codecool.shop.controller.ProductController;
+import com.codecool.shop.dao.SqliteJDBCCOnnector;
+import java.sql.SQLException;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        if(args.length > 0 && args[0].equals("--create-tables")) {
+            try {
+                SqliteJDBCCOnnector.createTables();
+            } catch (SQLException e) {
+                System.out.println("Cannot create tables in db.");
+            }
+        }
         ProductController productController = new ProductController();
         productController.listProducts();
     }
-
-
 }
