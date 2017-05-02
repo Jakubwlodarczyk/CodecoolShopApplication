a504f822c9f194f765b7ee3d919def94140642f1
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-02 13:26:31 +0200

    Changed displaying to use ProductView class

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,19 +3,19 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.view.ProductView;
 
 
 import java.util.List;
 
 public class ProductController {
+
+    private ProductDao productDao = new ProductDaoSqlite();
+    private List<Product> products = productDao.getAll();
+
     public void listProducts() {
-        ProductDao productDao = new ProductDaoSqlite();
-        List<Product> products = productDao.getAll();
-        for(Product p: products) {
-            System.out.println(p.getName());
+        ProductView view = new ProductView();
+        view.displayProductList(products);
         }
-
-    }
-
 
 }
