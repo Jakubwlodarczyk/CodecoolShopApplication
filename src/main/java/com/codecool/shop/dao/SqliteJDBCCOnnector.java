16a5c7f7d613c156c93f22280a1ccf9f731e4f65
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-02 15:13:47 +0200

    Add class responsible for db connection

---
+++ src/main/java/com/codecool/shop/dao/SqliteJDBCCOnnector.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.dao;
+
+import java.sql.*;
+
+public class SqliteJDBCCOnnector {
+
+    public static Connection connection() {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+        } catch (SQLException e) {
+            System.out.println("Connection to database failed.");
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void createTables() throws SQLException {
+
+        Connection connection = connection();
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS \"products\" (\n" +
+                "\t`id`\tINTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "\t`name`\tTEXT,\n" +
+                "\t`description`\tTEXT,\n" +
+                "\t`price`\tDOUBLE DEFAULT 0.00\n" +
+                ")");
+    }
+}
