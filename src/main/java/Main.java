cafcaeecafb258af2839841a3bb1eec82e5617b0
Krzychu <sk@in.pl>
2017-05-04 08:04:40 +0200

    After wednesday

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import com.codecool.shop.controller.ProductController;
+import com.codecool.shop.dao.SqliteJDBCConnector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
+        if(args.length > 0 && args[0].equals("--create-tables")) {
+            try {
+                SqliteJDBCConnector.createTables();
+            } catch (SQLException e) {
+                System.out.println("Cannot create tables in DB");
+                System.out.println(e);
+            }
+        }
 
+        ProductController productController = new ProductController();
+        productController.listProducts();
     }
 
 
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,13 +1,19 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.view.ProductView;
 
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class ProductController {
+    private ProductDao productDao = new ProductDaoSqlite();
+    private ProductView view = new ProductView();
 
+    public void listProducts() {
+        List<Product> products = productDao.getAll();
+        view.displayProductsList(products);
+    }
 
 }
