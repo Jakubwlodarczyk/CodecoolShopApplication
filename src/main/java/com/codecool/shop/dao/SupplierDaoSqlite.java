513bfa9fdd36e4137ec48f53ba880ded551727fd
Krzychu <sk@in.pl>
2017-05-04 15:01:41 +0200

    Implement blank methods in supplierdao

---
+++ src/main/java/com/codecool/shop/dao/SupplierDaoSqlite.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+/**
+ * Created by krzysiek on 5/4/17.
+ */
+public class SupplierDaoSqlite implements SupplierDao {
+    @Override
+    public void add(Supplier supplier) {
+
+    }
+
+    @Override
+    public Supplier find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        return null;
+    }
+}
