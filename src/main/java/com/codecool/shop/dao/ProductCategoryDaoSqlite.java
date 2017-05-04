94e950baa48ac0eb3cf36cbf2723f4e3ca0d2032
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-04 12:08:25 +0200

    Add product category dao sqlite class

---
+++ src/main/java/com/codecool/shop/dao/ProductCategoryDaoSqlite.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.ProductCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCategoryDaoSqlite implements ProductCategoryDao {
+
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return new ProductCategory("jakas", "category", "description");
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        List<ProductCategory> categories = new ArrayList<ProductCategory>();
+
+        for(int i=1; i<=3; i++) {
+            String name = "Category" + Integer.toString(i);
+            ProductCategory category = new ProductCategory(name, "dep", "desc");
+            category.setId(i);
+            categories.add(category);
+        }
+        return categories;
+    }
+}
