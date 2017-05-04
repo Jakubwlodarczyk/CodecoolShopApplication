e0d07eb9d51094fb90e28f0d293623b68bef44f9
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-04 12:06:42 +0200

    Add displaying product list by category method

--- src/main/java/com/codecool/shop/view/ProductView.java
+++ src/main/java/com/codecool/shop/view/ProductView.java
@@ -1,14 +1,22 @@
 package com.codecool.shop.view;
 
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 
 import java.util.List;
 
 public class ProductView {
     public void displayProductList(List<Product> products) {
+        System.out.println("Products list:");
         for(Product p: products) {
             System.out.println(p.getName());
-
         }
+    }
+    public void displayListProductByCategory(List<ProductCategory> categories) {
+        System.out.println("Products by category:");
+        for(ProductCategory c: categories) {
+            System.out.println(c.getId() + ". " + c.getName());
+        }
+
     }
 }
