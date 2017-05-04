8a0eb8200be7d8dd20a1e7a37df81ada47b44520
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-04 10:11:18 +0200

    Add menu options

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,45 @@
 import com.codecool.shop.controller.ProductController;
-import com.codecool.shop.dao.SqliteJDBCCOnnector;
+import com.codecool.shop.dao.SqliteJDBCConnector;
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.sql.SQLException;
+import java.util.Scanner;
 
 
 public class Main {
 
+    static ProductController productController = new ProductController();
+
     public static void main(String[] args) {
         if(args.length > 0 && args[0].equals("--create-tables")) {
             try {
-                SqliteJDBCCOnnector.createTables();
+                SqliteJDBCConnector.createTables();
             } catch (SQLException e) {
                 System.out.println("Cannot create tables in db.");
             }
         }
-        ProductController productController = new ProductController();
-        productController.listProducts();
+        System.out.println("Select option:");
+        System.out.println("1. List all products.");
+        System.out.println("2. List products by category.");
+        Scanner reader = new Scanner(System.in);
+        while(!reader.hasNextInt()) {
+            System.out.println("Invalid input.");
+            reader.next();
+        }
+        Integer option = reader.nextInt();
+        switch(option) {
+            case 1:
+                productController.listProducts();
+                break;
+            case 2:
+                productController.listProductByCategory();
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Option not found.");
+        }
+
+
     }
 }
