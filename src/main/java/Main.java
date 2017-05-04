1eada4552aedec3e9773d7cc09b4a04d855a4e59
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-04 11:07:10 +0200

    Moved menu to seperate class, add calling it's method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.codecool.shop.controller.MainMenuController;
 import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.dao.SqliteJDBCConnector;
 import com.sun.org.apache.xpath.internal.SourceTree;
@@ -8,8 +9,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    static ProductController productController = new ProductController();
-
     public static void main(String[] args) {
         if(args.length > 0 && args[0].equals("--create-tables")) {
             try {
@@ -18,27 +17,9 @@ public class Main {
                 System.out.println("Cannot create tables in db.");
             }
         }
-        System.out.println("Select option:");
-        System.out.println("1. List all products.");
-        System.out.println("2. List products by category.");
-        Scanner reader = new Scanner(System.in);
-        while(!reader.hasNextInt()) {
-            System.out.println("Invalid input.");
-            reader.next();
-        }
-        Integer option = reader.nextInt();
-        switch(option) {
-            case 1:
-                productController.listProducts();
-                break;
-            case 2:
-                productController.listProductByCategory();
-                break;
-            case 3:
-                break;
-            default:
-                System.out.println("Option not found.");
-        }
+        MainMenuController menu = new MainMenuController();
+        menu.mainMenuAction();
+
 
 
     }
