046f5b13dcdcc6b7e209e4623c2936e2d6fcbe7a
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-04 11:06:36 +0200

    Add listing product by category method

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,25 +1,33 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductCategoryDaoSqlite;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.view.ProductView;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductController {
 
     private ProductDao productDao = new ProductDaoSqlite();
+    private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
     private List<Product> products = productDao.getAll();
+    private List<ProductCategory> categories = productCategoryDao.getAll();
     static ProductView view = new ProductView();
 
     public void listProducts() {
-        System.out.println("Products list:");
         view.displayProductList(products);
         }
 
     public void listProductByCategory() {
-        System.out.println("Products by category:");
+        view.displayListProductByCategory(categories);
+
+
+
     }
 }
