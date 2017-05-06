e07d776841313555b2b5cfa1f61a29f1c1b613a8
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-06 16:27:38 +0200

    Clean code, add "this" before calling view object

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -12,32 +12,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductController {
-
     private ProductDao productDao = new ProductDaoSqlite();
     private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
     private SupplierDao supplierDao = new SupplierDaoSqlite();
-    private List<Product> products = productDao.getAll();
-    private List<Supplier> suppliers = supplierDao.getAll();
-    private List<ProductCategory> categories = productCategoryDao.getAll();
-    static ProductView view = new ProductView();
+    private ProductView view = new ProductView();
+
 
     public void listProducts() {
-        view.displayProductList(products);
+        List<Product> products = productDao.getAll();
+        this.view.displayProductList(products);
         }
 
     public void listProductByCategory() {
-        view.displayListProductByCategory(categories);
+        List<ProductCategory> categories = productCategoryDao.getAll();
+        this.view.displayListProductByCategory(categories);
+
         Integer categoryId = UserInput.getUserInput();
         ProductCategory productCategory = productCategoryDao.find(categoryId);
         List<Product> products = productDao.getBy(productCategory);
-        view.displayProductList(products);
+        this.view.displayProductList(products);
     }
 
     public void listProductsBySupplier() {
-        view.displayListProductBySupplier(suppliers);
+        List<Supplier> suppliers = supplierDao.getAll();
+        this.view.displayListProductBySupplier(suppliers);
+
         Integer supplierId = UserInput.getUserInput();
         Supplier supplier = supplierDao.find(supplierId);
         List<Product> products = productDao.getBy(supplier);
-        view.displayProductList(products);
+        this.view.displayProductList(products);
     }
 }
