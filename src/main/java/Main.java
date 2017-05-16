ce09332ab2475b75a71c12ea817d97b96af3dfbe
Jakub <kubawl.91@gmail.com>
2017-05-16 12:14:39 +0200

    delete unused imports from main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,4 @@
 import com.codecool.shop.Application;
-import com.codecool.shop.controller.MainMenuController;
-import com.codecool.shop.controller.ProductController;
-import com.codecool.shop.dao.SqliteJDBCConnector;
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-import java.sql.SQLException;
-import java.util.Scanner;
 
 
 public class Main {
