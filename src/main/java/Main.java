1560fc9113b159679d422daf8f62323d19aab618
Krzychu <sk@in.pl>
2017-05-18 15:27:17 +0200

    Add Ctrl+c handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,11 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                System.out.println("Bye bye :( ");
+            }
+        });
         new Application(args);
     }
 }
