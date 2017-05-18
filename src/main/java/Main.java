68aecec537d67fb69496495444004396c6c30c95
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-18 11:12:32 +0200

    Change calling new Application to getting app object via class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        new Application(args);
+        Application.getApplication(args);
     }
 }
