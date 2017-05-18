10a39fd254fed92ea1468df40d65c2d1e230e33c
Ania Polak <tunczykzcytryna@gmail.com>
2017-05-18 17:36:10 +0200

    Add method run to Application class

--- src/main/java/com/codecool/shop/Application.java
+++ src/main/java/com/codecool/shop/Application.java
@@ -2,7 +2,7 @@ package com.codecool.shop;
 
 import com.codecool.shop.controller.BasketController;
 import com.codecool.shop.controller.ProductController;
-import com.codecool.shop.dao.SqliteJDBCConnector;
+import com.codecool.shop.dao.TableCreator;
 import static spark.Spark.*;
 import java.io.IOException;
 import java.sql.Connection;
@@ -15,36 +15,29 @@ public class Application {
     private ProductController productController = new ProductController();
     private BasketController basketController = new BasketController();
 
-    private Application() {
+    private Application() throws SQLException {
+    }
+    public void run() {
         try {
-            this.getConnection();
+            this.setConnection();
             this.dispatchRoutes();
         } catch (SQLException e) {
             System.out.println("Application initialization failed");
             e.printStackTrace();
         }
     }
-
-    public void initializeTables() throws SQLException {
-        try {
-            SqliteJDBCConnector.dropTables();
-            SqliteJDBCConnector.createTables();
-            SqliteJDBCConnector.seedUpTablesWithDumpData();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void setConnection() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
     }
 
-    public void migrateTables() throws SQLException {
-        try {
-            SqliteJDBCConnector.createTables();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static Application getApplication() throws SQLException {
+        if (app == null) {
+            app = new Application();
         }
+        return app;
     }
 
     public Connection getConnection() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
         return connection;
     }
 
@@ -53,19 +46,28 @@ public class Application {
         staticFileLocation("/public");
         port(8888);
         get("/", (req, res) -> this.productController.renderListProducts(req, res));
-        get ("/basket", (req, res) -> this.basketController.renderListBasketItems(req, res));
-        post("/byCategory", (req, res) -> this.productController.renderListProductByCategory(req,res));
-        post("/bySupplier", (req, res) -> this.productController.renderListProductsBySupplier(req,res));
+        get("/basket", (req, res) -> this.basketController.renderListBasketItems(req, res));
+        post("/byCategory", (req, res) -> this.productController.renderListProductByCategory(req, res));
+        post("/bySupplier", (req, res) -> this.productController.renderListProductsBySupplier(req, res));
         post("/add-to-basket", (req, res) -> this.productController.addToBasket(req, res));
-
         post("/delete-from-basket", (req, res) -> this.productController.deleteFromBasket(req, res));
-
     }
 
-    public static Application getApplication() {
-        if(app == null) {
-            app = new Application();
+    public void initializeTables() throws SQLException {
+        try {
+            TableCreator.dropTables();
+            TableCreator.createTables();
+            TableCreator.seedUpTablesWithDumpData();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return app;
+    }
+
+    public void migrateTables() throws SQLException {
+        try {
+            TableCreator.createTables();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
