e2daa296a83c09bedbb3eff4c852679efc09f5e1
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-05-18 16:41:27 +0200

    add import java.oi.File , stop app if DB doesnt exists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 import com.codecool.shop.Application;
+
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
+        File databse = new File("/home/jakub/Java/codecool-shops-forkineye2/src/main/resources/database.db");
+        if(args.length == 0 && !databse.exists()){
+            Application.stopApplicationBoot();
+        }
         Application app = Application.getApplication();
         if(args.length>0){
             if (args[0].equals("--init-db")) {
