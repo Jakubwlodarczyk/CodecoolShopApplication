890c51109edc923e250102201b219274572a1b91
sebastiankurant <sebastian.kt7@gmail.com>
2017-05-29 13:55:17 +0200

    Test commit

--- tests/com/codecool/shop/model/BaseModelTest.java
+++ tests/com/codecool/shop/model/BaseModelTest.java
@@ -1,10 +1,15 @@
 package com.codecool.shop.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by pgurdek on 29.05.17.
  */
 class BaseModelTest {
+	@Test
+	public void testGetName(){
 
+	}
 }
